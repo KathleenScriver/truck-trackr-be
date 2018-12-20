df1bf655f24c32cd86220a2e430fa7dde6d67543
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-20 14:37:18 -0700

    Adds model test for brewery.

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -5,6 +5,7 @@ describe Brewery do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:address) }
     it { should validate_presence_of(:contact_name) }
-    it {}
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:email) }
   end
 end
