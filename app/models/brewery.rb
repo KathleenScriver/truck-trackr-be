ee72c172b76d31de7648e7c17d536b037be399a9
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-20 14:38:18 -0700

    Adds Brewery model attributes.

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,2 +1,7 @@
 class Brewery < ApplicationRecord
+  validates_presence_of :name,
+                        :address,
+                        :contact_name,
+                        :phone,
+                        :email
 end
