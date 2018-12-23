512731436eab3cb828cf553dff048b830b233fa6
abdullaqudrat <aqudrat630@gmail.com>
2018-12-23 14:38:47 -0700

    Adds brewery show action and serializer

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::BreweriesController < ApplicationController
     breweries = Brewery.all
     render json: BreweryIndexSerializer.new(breweries).serialized_json
   end
+
+  def show
+    brewery = Brewery.find(params[:id])
+    render json: BreweryShowSerializer.new(brewery).serialized_json
+  end
 end
