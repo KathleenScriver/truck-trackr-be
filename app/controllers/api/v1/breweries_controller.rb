2fa637372f1f384c41c4c874837be79c8c1db561
abdullaqudrat <aqudrat630@gmail.com>
2018-12-23 15:24:32 -0700

    Adds sad path logic in brewery show action

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -5,7 +5,11 @@ class Api::V1::BreweriesController < ApplicationController
   end
 
   def show
-    brewery = Brewery.find(params[:id])
-    render json: BreweryShowSerializer.new(brewery).serialized_json
+    brewery = Brewery.find_by_id(params[:id])
+    if brewery
+      render json: BreweryShowSerializer.new(brewery).serialized_json
+    else
+      render json: {message: "Brewery not found with that ID"}, status: 404
+    end
   end
 end
