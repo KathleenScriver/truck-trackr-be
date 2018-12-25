f077b4c4de2ecd7a874632f67a4cde2413fd07cf
abdullaqudrat <aqudrat630@gmail.com>
2018-12-25 01:23:34 -0700

    Changes food truck serializers
    Changes Food truck index serializer type
    Deletes truck city serializer(unused)

--- app/serializers/food_truck_index_serializer.rb
+++ app/serializers/food_truck_index_serializer.rb
@@ -1,4 +1,5 @@
 class FoodTruckIndexSerializer
   include FastJsonapi::ObjectSerializer
+  set_type :food_truck
   attributes :name
 end
--- app/serializers/truck_city_serializer.rb
+++
@@ -1,5 +0,0 @@
-class TruckCitySerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :city
-  belongs_to :food_truck
-end
