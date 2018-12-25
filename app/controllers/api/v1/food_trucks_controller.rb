b173dde5e7877bb2f14ddd49a8c2122bda371779
abdullaqudrat <aqudrat630@gmail.com>
2018-12-25 01:20:19 -0700

    Adds route and action

--- app/controllers/api/v1/food_trucks_controller.rb
+++ app/controllers/api/v1/food_trucks_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::FoodTrucksController < ApplicationController
     food_trucks = FoodTruck.all
     render json: FoodTruckIndexSerializer.new(food_trucks).serialized_json
   end
+
+  def show
+    food_truck = FoodTruck.find_by_id(params[:id])
+    render json: FoodTruckShowSerializer.new(food_truck).serialized_json
+  end
 end
