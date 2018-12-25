2c1034a6cf278d3ae97c9803946f21c8882ec6f3
abdullaqudrat <aqudrat630@gmail.com>
2018-12-25 11:47:47 -0700

    Adds happy/sad tests for brewery POST
    edits:
    - brewery show test to return id in message
    -changes serializer type to "brewery"

--- spec/requests/brewery_request_spec.rb
+++ spec/requests/brewery_request_spec.rb
@@ -31,7 +31,7 @@ describe("Brewery API") do
       expect(brewery_data[:data]).to have_key(:id)
       expect(brewery_data[:data][:id]).to eq(brewery.id.to_s)
       expect(brewery_data[:data]).to have_key(:type)
-      expect(brewery_data[:data][:type]).to eq("brewery_show")
+      expect(brewery_data[:data][:type]).to eq("brewery")
       expect(brewery_data[:data]).to have_key(:attributes)
       expect(brewery_data[:data][:attributes]).to have_key(:name)
       expect(brewery_data[:data][:attributes][:name]).to eq(brewery.name)
@@ -43,6 +43,10 @@ describe("Brewery API") do
       expect(brewery_data[:data][:attributes][:phone]).to eq(brewery.phone)
       expect(brewery_data[:data][:attributes]).to have_key(:email)
       expect(brewery_data[:data][:attributes][:email]).to eq(brewery.email)
+      expect(brewery_data[:data][:attributes]).to have_key(:website)
+      expect(brewery_data[:data][:attributes][:website]).to eq(brewery.website)
+      expect(brewery_data[:data][:attributes]).to have_key(:logo_image)
+      expect(brewery_data[:data][:attributes][:logo_image]).to eq(brewery.logo_image)
     end
     it("returns a 404 if brewery does not exist") do
 
@@ -51,7 +55,60 @@ describe("Brewery API") do
       brewery_data = JSON.parse(response.body, symbolize_names: true)
       expect(response.status).to eq(404)
       expect(brewery_data).to have_key(:message)
-      expect(brewery_data[:message]).to eq("Brewery not found with that ID")
+      expect(brewery_data[:message]).to eq("Brewery not found with ID 100")
+    end
+  end
+  describe("POST /breweries/:id") do
+    it("creates a brewery with all required attributes") do
+      payload = {
+        name: "Delirium",
+        address: "1156 Windsor St., Boulder, CO 80604",
+        contact_name: "Elliot",
+        phone: "555-555-5555",
+        email: "ddefault@mockweb.com",
+        website: "http://dddefaultmockwebsite.com"
+      }
+
+      post "/api/v1/breweries", params: payload
+
+      brewery_data = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(response).to be_successful
+      expect(brewery_data).to have_key(:data)
+      expect(brewery_data[:data].count).to eq(3)
+      expect(brewery_data[:data]).to have_key(:id)
+      expect(brewery_data[:data][:id]).to_not eq(nil)
+      expect(brewery_data[:data]).to have_key(:type)
+      expect(brewery_data[:data][:type]).to eq("brewery")
+      expect(brewery_data[:data]).to have_key(:attributes)
+      expect(brewery_data[:data][:attributes]).to have_key(:name)
+      expect(brewery_data[:data][:attributes][:name]).to eq(payload[:name])
+      expect(brewery_data[:data][:attributes]).to have_key(:address)
+      expect(brewery_data[:data][:attributes][:address]).to eq(payload[:address])
+      expect(brewery_data[:data][:attributes]).to have_key(:contact_name)
+      expect(brewery_data[:data][:attributes][:contact_name]).to eq(payload[:contact_name])
+      expect(brewery_data[:data][:attributes]).to have_key(:phone)
+      expect(brewery_data[:data][:attributes][:phone]).to eq(payload[:phone])
+      expect(brewery_data[:data][:attributes]).to have_key(:email)
+      expect(brewery_data[:data][:attributes][:email]).to eq(payload[:email])
+      expect(brewery_data[:data][:attributes]).to have_key(:website)
+      expect(brewery_data[:data][:attributes][:website]).to eq(payload[:website])
+    end
+    it("returns a 400 if brewery doesn't have all the required paramters") do
+      payload = {
+        address: "1156 Windsor St., Boulder, CO 80604",
+        contact_name: "Elliot",
+        phone: "555-555-5555",
+        email: "ddefault@mockweb.com",
+        website: "http://dddefaultmockwebsite.com"
+      }
+
+      post "/api/v1/breweries", params: payload
+
+      brewery_data = JSON.parse(response.body, symbolize_names: true)
+      expect(response.status).to eq(400)
+      expect(brewery_data).to have_key(:message)
+      expect(brewery_data[:message]).to eq("Failed")
     end
   end
 end
