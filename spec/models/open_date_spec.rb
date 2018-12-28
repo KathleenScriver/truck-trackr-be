4cc9ad240cc88a25537e033ecd7ba75b14313d4f
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-28 13:37:56 -0700

    Adds validation and relationship tests to open_date model.

--- spec/models/open_date_spec.rb
+++ spec/models/open_date_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe OpenDate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe('validations') do
+    it { should validate_presence_of(:date) }
+    it { should validate_presence_of(:booked?) }
+  end
+  describe('relationships') do
+    it { should belong_to(:food_truck) }
+  end
 end
