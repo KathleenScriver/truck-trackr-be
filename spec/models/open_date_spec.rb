d689c0171e7e119dd16e17a5b161db6d310a77db
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-28 13:57:50 -0700

    Adds validation for boolean column.

--- spec/models/open_date_spec.rb
+++ spec/models/open_date_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe OpenDate, type: :model do
   describe('validations') do
     it { should validate_presence_of(:date) }
-    it { should validate_presence_of(:booked?) }
   end
   describe('relationships') do
     it { should belong_to(:food_truck) }
