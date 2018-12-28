f2c7c42824018c29c3c5b505f64ee1f492930e51
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-28 13:40:48 -0700

    Adds open_date relationship to food_truck.

--- app/models/food_truck.rb
+++ app/models/food_truck.rb
@@ -2,4 +2,5 @@ class FoodTruck < ApplicationRecord
     validates_presence_of :name, :food_type, :contact_name, :phone, :email
     has_many :food_truck_cities
     has_many :cities, through: :food_truck_cities
+    has_many :open_dates 
 end
