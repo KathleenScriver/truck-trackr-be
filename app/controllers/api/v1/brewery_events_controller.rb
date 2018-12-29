be398b65774bcf22016901742743bb5277de309d
abdullaqudrat <aqudrat630@gmail.com>
2018-12-29 14:41:01 -0700

    Adds route and controller action for delete brewery event
    ATP

--- app/controllers/api/v1/brewery_events_controller.rb
+++ app/controllers/api/v1/brewery_events_controller.rb
@@ -35,6 +35,21 @@ class Api::V1::BreweryEventsController < ApplicationController
     end
   end
 
+  def destroy
+    brewery = Brewery.find_by_id(params[:brewery_id])
+    brewery_event = BreweryEvent.find_by_id(params[:id])
+    if brewery == nil
+      render json: {message: "Brewery not found with ID #{params[:brewery_id]}"}, status: 404
+    elsif brewery_event == nil
+      render json: {message: "Event not found with ID #{params[:id]}"}, status: 404
+    elsif brewery.id == brewery_event.brewery_id
+      brewery_event.destroy
+      render status: 204
+    else
+      render json: {message: "Event ID #{params[:id]} not associated with Brewery ID #{params[:brewery_id]}"}, status: 400
+    end
+  end
+
   private
 
   def brewery_event_params
