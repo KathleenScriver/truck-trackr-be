e41894d7171c46946d4febda778a37062082c8a8
Kathleen Yruegas <kathleenecn@gmail.com>
2018-12-31 12:28:08 -0700

    Adds CORS headers.

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module TruckTrackrApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => "GET,POST,OPTIONS"
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
