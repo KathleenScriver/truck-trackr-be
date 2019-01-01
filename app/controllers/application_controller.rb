dfe554d34be3585a453e8620777201c87599f8da
Kathleen Yruegas <kathleenecn@gmail.com>
2019-01-01 13:44:17 -0700

    Adds config to allow for sessions and cookies.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
+
+  protect_from_forgery :exception
+  before_action :set_csrf_cooke
+
+  private
+
+  def set_csrf_cookie
+    cookies["CSRF-TOKEN"] = form_authenticity_token
+  end
 end
