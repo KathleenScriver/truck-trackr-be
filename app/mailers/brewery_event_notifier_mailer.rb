b409a89cf91fc1978bc8b00e478454a18258d4b5
abdullaqudrat <aqudrat630@gmail.com>
2019-01-02 15:25:14 -0700

    Adds broadcast method in mailer

---
+++ app/mailers/brewery_event_notifier_mailer.rb
@@ -0,0 +1,13 @@
+class BreweryEventNotifierMailer < ApplicationMailer
+
+    def self.broadcast_setup(all_food_truck_emails, brewery_event)
+        all_food_truck_emails.each do |email|
+            broadcast(email, brewery_event)
+        end
+    end
+
+    def broadcast(email, brewery_event)
+        @brewery_event = brewery_event
+        mail(to: email, subject: "New Event Posted")
+    end
+end
