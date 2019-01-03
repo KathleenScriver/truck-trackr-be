15f2dbbcd7a0865be87049a4f5b6292929f498dd
Kathleen Yruegas <kathleenecn@gmail.com>
2019-01-03 12:05:44 -0700

    Adds test for login response.

--- spec/requests/login_requests_spec.rb
+++ spec/requests/login_requests_spec.rb
@@ -12,8 +12,16 @@ describe("Login Logout API") do
 
       post "/login", params: payload
 
+      logged_in = JSON.parse(response.body, symbolize_names: true)
+require "pry"; binding.pry
       expect(response).to be_successful
       expect(response.status).to eq(201)
+      expect(logged_in).to have_key(:data)
+      expect(logged_in[:data]).to have_key(:id.to_s)
+      expect(logged_in[:data][:id]).to eq(food_truck.id)
+      expect(logged_in[:data]).to have_key(:attributes)
+      expect(logged_in[:data][:attributes]).to have_key(:name)
+      expect(logged_in[:data][:attributes][:name]).to eq(food_truck.name)
     end
   end
 
