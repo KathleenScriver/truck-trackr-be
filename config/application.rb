cbbdeeccdb89fef08775b85ab348a2b49abf28a3
abdullaqudrat <aqudrat630@gmail.com>
2019-01-02 17:16:34 -0700

    Adds sidekiq and sinatra
    Adds configs

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module TruckTrackrApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    
+    config.active_job.queue_adapter = :sidekiq
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
