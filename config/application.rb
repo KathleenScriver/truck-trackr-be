8153559e36667694912c60393119e80f237e21c8
Kathleen Yruegas <kathleenecn@gmail.com>
2019-01-03 14:46:00 -0700

    cors

--- config/application.rb
+++ config/application.rb
@@ -33,13 +33,6 @@ module TruckTrackrApi
       end
     end
 
-    config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
-    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(","),
-    'Access-Control-Allow-Credentials' => true
-  }
-
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
