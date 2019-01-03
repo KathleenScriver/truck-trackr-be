e80a47d9eee581cc49c0a87e77a4ddf5b6a3a5e9
Kathleen Yruegas <kathleenecn@gmail.com>
2019-01-03 14:09:57 -0700

    CORS

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module TruckTrackrApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins "['localhost:3000', 'https://trucktrackr.herokuapp.com/']"
+        origins ['localhost:3000', 'https://trucktrackr.herokuapp.com/']
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :delete, :options, :head]
